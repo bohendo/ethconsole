e63dc30e9a734ccb956c043c2e91d60c98d12871
Bo Henderson <bohende@gmail.com>
2018-08-02 15:12:16 +0530

    Successful connection to ledger 🎉

--- src/sign.js
+++ src/sign.js
@@ -1,15 +1,28 @@
 const TransportNodeHid = require('@ledgerhq/hw-transport-node-hid').default
 const Eth = require('@ledgerhq/hw-app-eth').default
 
-console.log('Eth:', Eth)
-console.log('TransportNodeHid:', TransportNodeHid)
+global.HID = require('node-hid')
 
-const sign = () => {
-    console.log(`Connecting to wallet: Ledger`)
-    TransportNodeHid.create().then(transport => {
-        const wallet = new Eth(transport)
-        console.log(`transport: ${JSON.stringify(transport, null, 2)}`)
-        console.log(`wallet: ${JSON.stringify(wallet, null, 2)}`)
+var wallet
+const getWallet = () => {
+    if (wallet) {
+        console.log(`Got cached wallet`)
+        return Promise.resolve(wallet)
+    }
+    console.log(`Getting new wallet`)
+    wallet = TransportNodeHid.create(HID.HID).then(transport => new Eth(transport))
+    return wallet
+}
+
+const sign = (address_index) => {
+    return getWallet().then(wallet => {
+        const myWallet = wallet.getAddress(`44'/60'/${address_index}'/0/0`)
+        return myWallet.then(address => {
+            console.log(`Got address: ${JSON.stringify(address.address)}`)
+            return address
+        }).catch(error => {
+            console.log(`Failed to access ledger, please plug-in or unlock device: ${error}`)
+        })
     }).catch((error) => {
         console.log(`Failed to access ledger, please plug-in or unlock device: ${error}`)
     })
