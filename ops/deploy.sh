2557f80b21d861e88ddab6f151ebf7c54ac9473f
Bo Henderson <bohende@gmail.com>
2018-08-04 09:43:24 +0530

    Tweak deploy script to enable loading extra tools

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -2,4 +2,9 @@
 
 DIR="`cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd`"
 
-node --no-deprecation --interactive --require $DIR/../src/entry.js
+if [[ -f "$1" ]]
+then
+    node --no-deprecation --interactive --require $DIR/../src/entry.js --require $1
+else
+    node --no-deprecation --interactive --require $DIR/../src/entry.js
+fi
