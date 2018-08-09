9260a6513e76a1b4b3a22b6850ce6391805916be
Bo Henderson <bohende@gmail.com>
2018-08-09 18:41:05 +0530

    Add syncing indicator to console welcome message

--- src/web3.js
+++ src/web3.js
@@ -3,14 +3,21 @@ const Web3 = require('web3')
 console.log(`Connecting to ${process.env.ETH_PROVIDER}`)
 const web3 = new Web3(new Web3.providers.WebsocketProvider(process.env.ETH_PROVIDER))
 
-web3.eth.getBlock('latest').then(response => {
-    if (response.number) {
-        console.log(`Connection to ethereum provider success, synced to block: ${response.number}`)
+web3.eth.getBlock('latest').then(block => {
+    if (block.number || typeof block.number === 'number') {
+        console.log(`Connection to ethereum provider succeeded`)
+        web3.eth.isSyncing().then(isSyncing => {
+            if (isSyncing) {
+                console.log(`Provider is syncing, currently on block ${isSyncing.currentBlock}, highest block: ${isSyncing.highestBlock}, blocks to go: ${isSyncing.highestBlock - isSyncing.currentBlock}`)
+            } else {
+                console.log(`Provider is up to date, latest block: ${block.number}`)
+            }
+        })
     } else {
-        console.log(`Connection to ethereum provider failed`)
+        console.log(`Connection to ethereum provider failed: ${JSON.stringify(block)}`)
     }
 }).catch(response => {
-    console.log(`Connection to ethereum provider failed`)
+    console.log(`Connection to ethereum provider failed: ${response}`)
 })
 
 module.exports = web3
