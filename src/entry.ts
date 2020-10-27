7bd589071d40d0b0b05a399d0d6aa4b87e30e63c
bohendo <bohende@gmail.com>
2020-10-27 11:59:01 +0530

    🛠  get ledger wallet properly hooked up

--- src/entry.ts
+++ src/entry.ts
@@ -1,21 +1,23 @@
 import * as eth from "ethers";
 
-import { ledger } from "./ledger";
+import { ledger, getLedgerSigner } from "./ledger";
 import { log, provider, wallet, toHumanReadable } from "./utils";
 
-import * as yearn from "./yearn";
 import * as maker from "./maker";
+import * as yearn from "./yearn";
 
 // Attach exported utils to global for easy access in the console
 const setGlobal = (key: string, value: any): void => {
   (global as any)[key] = value;
 };
 
-setGlobal("yearn", yearn);
 setGlobal("maker", maker);
+setGlobal("yearn", yearn);
+
 setGlobal("toHumanReadable", toHumanReadable);
 setGlobal("eth", eth);
 setGlobal("ledger", ledger);
+setGlobal("getLedgerSigner", getLedgerSigner);
 setGlobal("log", log);
 setGlobal("provider", provider);
 setGlobal("wallet", wallet);
--- src/ledger.ts
+++ src/ledger.ts
@@ -1,46 +1,10 @@
-import TransportNodeHid from "@ledgerhq/hw-transport-node-hid";
-import Eth from "@ledgerhq/hw-app-eth";
+import { LedgerSigner } from "@ethersproject/hardware-wallets";
+
+import { provider } from "./utils";
 
 const hdWalletPath = index => `44'/60'/${index}'/0/0`
 
-const ledgerError = (msg) => {
-    throw(msg) // Bubble up to calling function's error handler
-}
+export const getLedgerSigner = (index: number): LedgerSigner =>
+  new LedgerSigner(provider, "hid", hdWalletPath(index));
 
-var ledgerDevice
-
-const getLedgerDevice = () => {
-  if (ledgerDevice) {
-    return Promise.resolve(ledgerDevice)
-  }
-  ledgerDevice = TransportNodeHid.create().then(transport => new Eth(transport))
-  return ledgerDevice
-}
-
-const sign = (index, hexData, signFunction) => {
-    return getLedgerDevice().then(ledgerDevice => {
-        return ledgerDevice[signFunction](hdWalletPath(index), hexData).then(signature => {
-            return signature
-        }).catch(ledgerError)
-    }).catch(ledgerError)
-}
-
-export const ledger = {
-
-  getAddress: (index) => {
-    return getLedgerDevice().then(ledgerDevice => {
-        return ledgerDevice.getAddress(hdWalletPath(index)).then(address => {
-            return address.address
-        }).catch(ledgerError)
-    }).catch(ledgerError)
-  },
-
-  signTx: (index, hexTx) => {
-    return sign(index, hexTx, 'signTransaction')
-  },
-
-  signMsg: (index, hexMsg) => {
-    return sign(index, hexMsg, 'signPersonalMessage')
-  },
-
-}
+export const ledger = getLedgerSigner(0);
