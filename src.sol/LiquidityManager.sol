adf3752807ee78cd5141d4d11eb2c8772c33f118
bohendo <bohendo@pm.me>
2021-01-27 16:06:30 +0530

    init liquidity manager contract + deployment

---
+++ src.sol/LiquidityManager.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.1;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LiquidityManager is Ownable {
+  constructor(uint s) {}
+}
