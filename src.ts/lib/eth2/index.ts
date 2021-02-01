a904b9045b5e3ef809341848922803e74d13667f
bohendo <bohendo@pm.me>
2021-02-01 19:16:58 +0530

    rm eth2 deposit contract

--- src.ts/lib/eth2/index.ts
+++
@@ -1,9 +0,0 @@
-import { Contract } from "@ethersproject/contracts";
-
-import { provider } from "../../constants";
-
-import depositAbi from "./Deposit.json";
-
-// TODO: what should we do about ens names on localnet?
-// resolved address: "0x00000000219ab540356cBB839Cbe05303d7705Fa"
-export const eth2Deposit = new Contract("depositcontract.eth", depositAbi, provider);
