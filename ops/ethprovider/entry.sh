d4462255b9bf896e040c5e303498264f1aea5a4d
bohendo <bohendo@pm.me>
2021-02-01 19:39:05 +0530

    update localhost deployments

--- ops/ethprovider/entry.sh
+++ ops/ethprovider/entry.sh
@@ -3,11 +3,13 @@ set -e
 
 export ETHCONSOLE_HARDHAT=true
 
-hardhat node --hostname 0.0.0.0 --port 8545  \
-  | pino-pretty --colorize --ignore module,pid,hostname &
+hardhat node --hostname 0.0.0.0 --port 8545 --no-deploy &
 pid=$!
 
 echo "Waiting for localnet evm instance to wake up (pid=$pid)"
 wait-for -q -t 30 localhost:8545 2>&1 | sed '/nc: bad address/d'
+echo "Good morning!"
+
+hardhat --network localhost deploy --write true | pino-pretty --colorize --ignore module,pid,hostname
 
 wait $pid
