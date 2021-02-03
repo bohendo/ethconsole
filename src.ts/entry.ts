54252f5e505a4100b62ca571ae2e7c908085bf29
bohendo <bohendo@pm.me>
2021-02-03 14:49:01 +0530

    add ethers constants & utils to console's global scope

--- src.ts/entry.ts
+++ src.ts/entry.ts
@@ -7,26 +7,13 @@ import { initLiq } from "./initLiq";
 import { ledger, getLedgerSigner } from "./ledger";
 import { log, sqrt, toHumanReadable } from "./utils";
 
-let network;
-if (env.ethProviderUrl.includes("localhost")) {
-  network = "localhost";
-} else if (env.ethProviderUrl.includes("mainnet")) {
-  network = "mainnet";
-} else {
-  network = "mainnet";
-}
-console.log(`Connected to ${network} provider ${env.ethProviderUrl.split("/").splice(0, 3).join("/")}`);
+////////////////////////////////////////
+/// Add various utils & constants to the global scope
 
 // Attach exported utils to global for easy access in the console
 const setGlobal = (key: string, value: any): void => {
   (global as any)[key] = value;
 };
-
-for (const key of Object.keys(deployments[network])) {
-  console.log(`Loading ${key} contract`);
-  const deployment = deployments[network][key];
-  setGlobal(key, new Contract(deployment.address, deployment.abi, provider));
-}
 
 setGlobal("BN", eth.BigNumber.from);
 setGlobal("eth", eth);
@@ -38,3 +25,30 @@ setGlobal("provider", provider);
 setGlobal("sqrt", sqrt);
 setGlobal("toHumanReadable", toHumanReadable);
 setGlobal("wallets", wallets);
+
+for (const constant of Object.keys(eth.constants)) {
+  setGlobal(constant, eth.constants[constant]);
+}
+
+for (const util of Object.keys(eth.utils)) {
+  setGlobal(util, eth.utils[util]);
+}
+
+////////////////////////////////////////
+/// Add deployed contract instances to the global scope
+
+let network;
+if (env.ethProviderUrl.includes("localhost")) {
+  network = "localhost";
+} else if (env.ethProviderUrl.includes("mainnet")) {
+  network = "mainnet";
+} else {
+  network = "mainnet";
+}
+console.log(`Connected to ${network} provider ${env.ethProviderUrl.split("/").splice(0, 3).join("/")}`);
+
+for (const key of Object.keys(deployments[network])) {
+  console.log(`Loading ${key} contract`);
+  const deployment = deployments[network][key];
+  setGlobal(key, new Contract(deployment.address, deployment.abi, provider));
+}
